34971f6312f8118a00085eb8eaa7c076cbc733b7
Janani M <janani.m@pptssolutions.onmicrosoft.com>
2024-06-20 20:11:59 +0530

    updated code

--- src/routes/protectedRoute.js
+++ src/routes/protectedRoute.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const verifyToken = require('../middleware/authMiddlewares');
 const deviceController = require('../Controllers/deviceController');
 
-router.post('/device', verifyToken, deviceController.deviceFlow);
+router.post('/device', verifyToken.verifyToken, deviceController.deviceFlow);
 
 module.exports = router;
