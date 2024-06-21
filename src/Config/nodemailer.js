a1ef3a317163204205c7c11bac58762e5e19d68c
Janani M <janani.m@pptssolutions.onmicrosoft.com>
2024-06-21 17:34:01 +0530

    implemented nodemailer

---
+++ src/Config/nodemailer.js
@@ -0,0 +1,24 @@
+// const nodemailer = require('nodemailer');
+
+// const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         user: '',
+//         pass: ''
+//     }
+// });
+
+// const mailOptions = {
+//     from: '',
+//     to: 'kevov58519@lisoren.com',
+//     subject: 'Testing mail',
+//     text: 'Hello from nodejs'
+// };
+
+// transporter.sendMail(mailOptions, (error, info) => {
+//     if (error) {
+//         console.error(`Error:`, error.message);
+//     } else {
+//         console.log(`Email send `, info.message);
+//     }
+// });
